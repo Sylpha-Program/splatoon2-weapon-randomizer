ced0d04bc6c1aa11e642e2c5c3aca574c42aa5dd
Sylpha-Program <sylpha.program@gmail.com>
2021-07-14 18:14:14 +0900

    fix top page

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,11 +4,13 @@ class ToppagesController < ApplicationController
     if session[:main_weapon_id]
       @main_weapon = MainWeapon.find_by(id: session[:main_weapon_id])
       if session[:user_id]
-        @total_point = Score.where(user_id: session[:user_id]).sum(:point)
-        @level = Math.sqrt(@total_point).floor
-        @next_level = (@level + 1) ** 2 - @total_point
         @gear_set = GearSet.find_by(user_id: session[:user_id], main_weapon_id: session[:main_weapon_id])
       end
+    end
+    if session[:user_id]
+      @total_point = Score.where(user_id: session[:user_id]).sum(:point)
+      @level = Math.sqrt(@total_point).floor
+      @next_level = (@level + 1) ** 2 - @total_point
     end
   end
   
