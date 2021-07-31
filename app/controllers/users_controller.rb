0ee82ab179d15a233f39f4e67fbb0b9a2cc56143
Sylpha-Program <sylpha.program@gmail.com>
2021-07-31 13:00:05 +0900

    add achievement rate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @total_point = Score.where(user_id: session[:user_id]).sum(:point)
     @level = Math.sqrt(@total_point).floor
     @next_level = (@level + 1) ** 2 - @total_point
+    @achievement_rate = ((Score.where(user_id: session[:user_id]).where.not(point: 0).count.to_f / Score.where(user_id: session[:user_id]).count.to_f) * 100).round(2)
   end
 
   def new
