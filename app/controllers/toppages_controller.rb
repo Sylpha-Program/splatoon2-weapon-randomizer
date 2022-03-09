2e7e29c02b4c252ac3b43dc165162baa6d99fbb2
Sylpha-Program <sylpha.program@gmail.com>
2022-03-09 17:26:56 +0900

    update toppages index

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -8,6 +8,24 @@ class ToppagesController < ApplicationController
         @level = Math.sqrt((Score.where(user_id: session[:user_id], main_weapon_id: session[:main_weapon_id]).sum(:total_point)) / 100).floor
       end
     end
+    if session[:user_id]
+      @max_point = 0
+      @max_point_weapon = MainWeapon.find_by(id: session[:main_weapon_id])
+      @min_point = 5000
+      @min_point_weapon = MainWeapon.find_by(id: session[:main_weapon_id])
+      @main_weapons = MainWeapon.order(id: :asc)
+      @main_weapons.each do |main_weapon|
+        @sum_total_point = Score.where(user_id: session[:user_id], main_weapon_id: main_weapon.id).sum(:total_point)
+        if @sum_total_point > @max_point
+          @max_point = @sum_total_point
+          @max_point_weapon = main_weapon
+        end
+        if @sum_total_point < @min_point
+          @min_point = @sum_total_point
+          @min_point_weapon = main_weapon
+        end
+      end
+    end
   end
   
 end
