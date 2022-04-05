f627b76c8d781758ff63137797c914b1167588a1
Sylpha-Program <sylpha.program@gmail.com>
2022-04-05 11:56:29 +0900

    update main weapons controller

--- app/controllers/main_weapons_controller.rb
+++ app/controllers/main_weapons_controller.rb
@@ -2,14 +2,14 @@ class MainWeaponsController < ApplicationController
 
   def index
     @main_weapons = MainWeapon.order(id: :asc).page(params[:page]).per(66)
-    @score = Score.find_by(user_id: session[user_id], main_weapon_id: 13, stage_id: 10)
-    @score.max_point = 1044
-    @score.total_point = 1925
-    @score.save
-    @score = Score.find_by(user_id: session[user_id], main_weapon_id: 13, stage_id: 12)
-    @score.max_point = 0
-    @score.total_point = 0
-    @score.save
+    @score1 = Score.find_by(user_id: session[user_id], main_weapon_id: 13, stage_id: 10)
+    @score1.max_point = 1044
+    @score1.total_point = 1925
+    @score1.save
+    @score2 = Score.find_by(user_id: session[user_id], main_weapon_id: 13, stage_id: 12)
+    @score2.max_point = 0
+    @score2.total_point = 0
+    @score2.save
   end
 
   def show
